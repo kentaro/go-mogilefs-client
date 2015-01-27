8ccc11d53f0aad7152ea8a714799b9f788708b1a
Kentaro Kuribayashi <kentarok@gmail.com>
2015-01-27 19:59:00 +0900

    Wait port for starting

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package mogilefs
 
 import (
+	"github.com/lestrrat/go-tcputil"
+	"log"
 	"os"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
@@ -15,6 +18,20 @@ func TestMain(m *testing.M) {
 
 	defer exec.Command("pkill", "-f", "mogilefsd").Run()
 	defer exec.Command("pkill", "-f", "mogstored").Run()
+
+	var err error
+	err = tcputil.WaitLocalPort(7001, 30*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tcputil.WaitLocalPort(7500, 30*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tcputil.WaitLocalPort(7501, 30*time.Second)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	exitCode = m.Run()
 }
